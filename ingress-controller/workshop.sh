9f1d0e3ecacd0e54f31b8e85a4d0297ff17a9c31
Bitandxc <55730009+Bitandxc@users.noreply.github.com>
2019-11-14 15:05:13 +0530

    Update workshop.sh

--- ingress-controller/workshop.sh
+++ ingress-controller/workshop.sh
@@ -2,7 +2,7 @@
 
 # Enable the HTTP routing add-on on your cluster
 # This could be enabled in the Azure Portal, also.
-az aks enable-addons --resource-group akschallenge --name <unique-aks-cluster-name> --addons http_application_routing
+az aks enable-addons --resource-group akschallenge --name aksdemo-rg --addons http_application_routing
 
 # Retrieve your cluster specific DNS zone name by running the command below
-az aks show --resource-group aks-k8s-extia-rg --name aks-k8s-extia --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
+az aks show --resource-group aksdemo-rg --name devops --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
